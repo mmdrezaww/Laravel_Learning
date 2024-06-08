b2fbe75b515755c107c554df458ba9e95df9eff9
mamzaw <gmohammadreza867@gmail.com>
2024-06-09 01:03:33 +0330

    Install Laravel Ui package & Creat User

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Models\student;
+use App\Models\User;
 
 
 Route::get('/', function () {
@@ -16,6 +17,24 @@ Route::get('/students', function () {
     return \App\Models\student::all();
 });
 
+//Start_First_Route
+
+Route::get('/student{student}', function ($id) {
+    return student::where('id', $id)->first();
+});
+
+//End_First_Route
+
+//Start_Easy_Route
 Route::get('/student/{student}', function (student $student) {
     return $student;
 });
+
+//End_Easy_Route
+Route::get('/myuser/{user}', function (User $user) {
+    return $user;
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
